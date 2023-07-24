26462e8edd97344440db6bc32e9c45e7499dc7a6
Your Name <you@example.com>
2023-07-24 18:37:26 +0200

    final final

--- BestCalculator/Program.cs
+++ BestCalculator/Program.cs
@@ -4,35 +4,42 @@ Console.WriteLine("******************* Calculator ****************");
 
 
 Calculations calculations = new Calculations();
-
-Console.WriteLine("Enter two numbers");
-double num1 = Convert.ToDouble(Console.ReadLine());
-double num2 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("*************************************************");
-
-Console.WriteLine("Enter symbol (+,-,*,/):\n");
-string symbol = Console.ReadLine();
-
-Console.WriteLine("*****************************************************");
-switch (symbol)
+while (true)
 {
-    case "+":
-        calculations.Addition(num1, num2);
+    Console.WriteLine("Enter two numbers");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+    double num2 = Convert.ToDouble(Console.ReadLine());
 
-        break;
-    case "-":
-        calculations.Substraction(num1, num2);
-        break;
+    Console.WriteLine("*************************************************");
 
-    case "*":
-        calculations.Multiplication(num1, num2);
-        break;
+    Console.WriteLine("Enter symbol (+,-,*,/):\n");
+    string symbol = Console.ReadLine();
 
-    case "/":
-        calculations.Division(num1, num2);
-        break;
+    Console.WriteLine("*****************************************************");
 
-     default:
-        break;
+    switch (symbol)
+    {
+        case "+":
+            calculations.Addition(num1, num2);
+
+            break;
+        case "-":
+            calculations.Substraction(num1, num2);
+            break;
+
+        case "*":
+            calculations.Multiplication(num1, num2);
+            break;
+
+        case "/":
+            calculations.Division(num1, num2);
+            break;
+
+        default:
+            Console.WriteLine("Incorrect operation");
+            break;
+    }
+
+    Console.ReadLine();
+
 }
