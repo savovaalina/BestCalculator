36be503bbdeda3352999b64ab0fc1d0e2a256f3e
Your Name <you@example.com>
2023-07-24 09:09:30 +0200

    final solution

---
+++ BestCalculator/Calculations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestCalculator
+{
+    public class Calculations
+    {
+        
+        public void Addition(double num1, double num2)
+        {
+
+            var rezult = num1 + num2;
+
+            Console.WriteLine($"Result of two numbers is: {rezult}");
+        }
+        public void Substraction(double num1, double num2)
+        {
+            var rezult = num1 - num2;
+            Console.WriteLine($"Result of two numbers is: {rezult}");
+        }
+
+        public void Multiplication(double num1, double num2)
+        {
+            var rezult = num1 * num2;
+            Console.WriteLine($"Result of two numbers is: {rezult}");
+        }
+        public void Division(double num1, double num2)
+        {
+            var rezult = num1 / num2;
+            Console.WriteLine($"Result of two numbers is: {rezult}");
+        }
+   
+         
+        }
+}
---
+++ BestCalculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using BestCalculator;
+
+/*
+Console.WriteLine("******************* Calculator ****************");
+
+
+Calculations calculations = new Calculations();
+
+Console.WriteLine("Enter first number");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Enter second number");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("*************************************************");
+
+Console.WriteLine("Enter symbol (+,-,*,/):\n");
+string symbol = Console.ReadLine();
+
+Console.WriteLine("*****************************************************");
+*/
+
+Console.WriteLine("******************* Calculator ****************");
+
+
+Calculations calculations = new Calculations();
+
+Console.WriteLine("Enter two numbers");
+double num1 = Convert.ToDouble(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("*************************************************");
+
+Console.WriteLine("Enter symbol (+,-,*,/):\n");
+string symbol = Console.ReadLine();
+
+Console.WriteLine("*****************************************************");
+switch (symbol)
+{
+    case "+":
+        calculations.Addition(num1, num2);
+
+        break;
+    case "-":
+        calculations.Substraction(num1, num2);
+        break;
+
+    case "*":
+        calculations.Multiplication(num1, num2);
+        break;
+
+    case "/":
+        calculations.Division(num1, num2);
+        break;
+
+     default:
+        break;
+}
