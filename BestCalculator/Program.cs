12846f0f196acc01ecb82b84b65c233c585b9686
Your Name <you@example.com>
2023-07-24 09:11:43 +0200

    final solutionn

--- BestCalculator/Program.cs
+++ BestCalculator/Program.cs
@@ -1,27 +1,5 @@
 ﻿using BestCalculator;
 
-/*
-Console.WriteLine("******************* Calculator ****************");
-
-
-Calculations calculations = new Calculations();
-
-Console.WriteLine("Enter first number");
-double num1 = Convert.ToDouble(Console.ReadLine());
-
-
-Console.WriteLine("Enter second number");
-double num2 = Convert.ToDouble(Console.ReadLine());
-
-
-Console.WriteLine("*************************************************");
-
-Console.WriteLine("Enter symbol (+,-,*,/):\n");
-string symbol = Console.ReadLine();
-
-Console.WriteLine("*****************************************************");
-*/
-
 Console.WriteLine("******************* Calculator ****************");
 
 
